14113f5d98df24ae5af45a7b838c853bd17742c0
wiktorw <wiktorwilk95@gmail.com>
2026-02-23 08:00:33 +0100

    Fix: Changed app.controller.spec.ts

--- apps/core_microservice/src/app.controller.spec.ts
+++ apps/core_microservice/src/app.controller.spec.ts
@@ -1,9 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
 
 describe('AppController', () => {
-  let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
@@ -11,12 +10,5 @@ describe('AppController', () => {
       providers: [AppService],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
-  });
-
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
-    });
   });
 });
