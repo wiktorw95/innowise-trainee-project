bef886600b824f5494c6eb66e537557e9394a775
wiktorw <wiktorwilk95@gmail.com>
2026-02-23 10:02:20 +0100

    Core: Setup NestJS application with database entities and Code First migrations

---
+++ apps/core_microservice/src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { AppController } from './app.controller.js';
+import { AppService } from './app.service.js';
+import { PrismaService } from './prisma.service.js';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes variables available in PrismaService
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService, PrismaService],
+})
+export class AppModule {}
---
+++ apps/core_microservice/src/prisma.service.ts
@@ -0,0 +1,44 @@
+import {
+  Injectable,
+  OnModuleInit,
+  OnModuleDestroy,
+  Logger,
+} from '@nestjs/common';
+import { PrismaClient } from '../generated/prisma/client.js';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  private readonly logger = new Logger(PrismaService.name);
+
+  constructor() {
+    const url = process.env.DATABASE_URL;
+    if (!url) {
+      throw new Error('DATABASE_URL environment variable is missing!');
+    }
+
+    const pool = new Pool({ connectionString: url });
+    const adapter = new PrismaPg(pool);
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    try {
+      await this.$connect();
+      await this.$executeRaw`SELECT 1`; // Verified handshake
+      this.logger.log(
+        'Successfully connected to PostgreSQL via Driver Adapter',
+      );
+    } catch (error) {
+      this.logger.error('Database connection failed', error);
+    }
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect();
+  }
+}
