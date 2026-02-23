97e6be69f2b17eab1d7755198ebfb247fa76d208
wiktorw <wiktorwilk95@gmail.com>
2026-02-23 08:03:01 +0100

    Fix: fixed app.controller.spec.ts v2

--- apps/core_microservice/src/app.controller.spec.ts
+++ apps/core_microservice/src/app.controller.spec.ts
@@ -1,14 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { PrismaService } from './prisma.service.js';
 
 describe('AppController', () => {
+  let appController: AppController;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [
+        AppService,
+        {
+          provide: PrismaService,
+          useValue: {
+            $queryRaw: jest.fn().mockResolvedValue([{ now: new Date() }]),
+          },
+        },
+      ],
     }).compile();
+    appController = app.get<AppController>(AppController);
+  });
 
+  it('should be defined', () => {
+    expect(appController).toBeDefined();
   });
 });
