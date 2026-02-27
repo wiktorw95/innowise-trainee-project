f138d569f0538bd0586a0fb5209d2037ef2ac770
wiktorw <wiktorwilk95@gmail.com>
2026-02-27 10:04:19 +0100

    Patch: Patched ci.yml file

--- apps/core_microservice/src/prisma/prisma.service.ts
+++ apps/core_microservice/src/prisma/prisma.service.ts
@@ -4,7 +4,7 @@ import {
   OnModuleDestroy,
   Logger,
 } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../../generated/prisma/client.js';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { Pool } from 'pg';
 
